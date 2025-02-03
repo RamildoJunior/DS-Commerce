69c8edd474c6967603d6a49a4834651a0a781820
RamildoJunior <rpsjuninho@gmail.com>
2025-02-03 17:23:29 -0300

    first commit

---
+++ src/test/java/com/devspring/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devspring.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
