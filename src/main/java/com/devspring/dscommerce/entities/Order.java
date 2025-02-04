90fe4e50a2619eba06648291fe30767067e0967d
RamildoJunior <rpsjuninho@gmail.com>
2025-02-04 17:10:49 -0300

    Payment

--- src/main/java/com/devspring/dscommerce/entities/Order.java
+++ src/main/java/com/devspring/dscommerce/entities/Order.java
@@ -12,14 +12,30 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
 
+    private OrderStatus status;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order(){
+
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
 
     public Long getId() {
         return id;
@@ -53,12 +69,6 @@ public class Order {
         this.client = client;
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
-        this.id = id;
-        this.moment = moment;
-        this.status = status;
-        this.client = client;
-    }
 
     @Override
     public boolean equals(Object o) {
