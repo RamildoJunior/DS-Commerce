17968202f1357230b364c1b0b0824bf49b5caaef
RamildoJunior <rpsjuninho@gmail.com>
2025-02-06 15:28:05 -0300

    corrigindo erro na clase OrdermItem

--- src/main/java/com/devspring/dscommerce/entities/Order.java
+++ src/main/java/com/devspring/dscommerce/entities/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -74,6 +75,22 @@ public class Order {
         this.client = client;
     }
 
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
+    }
 
     @Override
     public boolean equals(Object o) {
