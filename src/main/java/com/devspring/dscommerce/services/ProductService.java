c885efedcb061f42de7108388889be2d2c57c512
RamildoJunior <rpsjuninho@gmail.com>
2025-04-19 23:04:12 -0300

    Subindo correções

---
+++ src/main/java/com/devspring/dscommerce/services/ProductService.java
@@ -0,0 +1,12 @@
+package com.devspring.dscommerce.services;
+import com.devspring.dscommerce.dto.ProductDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public ProductDTO findById(Long id){
+        
+    }
+}
+
